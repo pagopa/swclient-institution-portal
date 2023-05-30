07193eaa5e4525751fd0e6ea5233dd14b9c2ee77
Arvark01 <anislucidi@gmail.com>
2023-05-30 15:59:53 +0200

    fixes

--- fe/src/components/VerticalNav/VerticalNav.tsx
+++ fe/src/components/VerticalNav/VerticalNav.tsx
@@ -1,58 +1,65 @@
 import React, { useState } from 'react';
 import {
-    List,
-    ListItemIcon,
-    ListItemText,
-    Box,
-    ListItemButton,
-} from "@mui/material";
+	List,
+	ListItemIcon,
+	ListItemText,
+	Box,
+	ListItemButton,
+} from '@mui/material';
 import { Link } from 'react-router-dom';
-
+import { theme } from '@pagopa/mui-italia';
 
 export interface Section {
-    title: string;
-    path: string;
-    active: boolean
-    element: JSX.Element
-    icon: JSX.Element
+	title: string;
+	path: string;
+	active: boolean;
+	element: JSX.Element;
+	icon: JSX.Element;
 }
 export interface VerticalNavProps {
-    sections: Array<Section>;
-    setSections: (sec: Array<Section>) => void;
-    currentPath: any;
+	sections: Array<Section>;
+	setSections: (sec: Array<Section>) => void;
+	currentPath: any;
 }
 
+export const VerticalNav = ({
+	sections,
+	setSections,
+	currentPath,
+}: VerticalNavProps) => {
+	const [selectedIndex, setSelectedIndex] = useState(0);
+	const handleListItemClick = (index: number) => {
+		setSelectedIndex(index);
+	};
 
-export const VerticalNav = ({ sections, setSections, currentPath }: VerticalNavProps) => {
-    const [selectedIndex, setSelectedIndex] = useState(0);
-
-    const handleListItemClick = (index: number) => {
-        setSelectedIndex(index);
-    };
-
-    return (
-        <Box sx={{ width: '100%', maxWidth: 360, float: 'left', marginRight: "10vw", backgroundColor: 'background.paper' }}>
-
-            <List component="nav" aria-label="main piattaforma-notifiche sender">
-                {
-                    sections.map((section, index) => (
-                        <Link to={section.path} key={index} style={{ textDecoration: 'none' }}>
-                            <ListItemButton
-                                selected={selectedIndex === index}
-                                onClick={() => handleListItemClick(index)}
-                            >
-                                <ListItemIcon>
-
-                                    {section.icon}
-                                </ListItemIcon>
-                                <ListItemText primary={section.title} />
-                            </ListItemButton>
-                        </Link>))
-                }
-
-            </List>
-        </Box>
-    );
-}
-
-
+	return (
+		<Box
+			sx={{
+				width: '100%',
+				maxWidth: 360,
+				float: 'left',
+				marginRight: '25.25vw',
+				backgroundColor: 'background.default',
+				height: '100vh',
+			}}
+		>
+			<List component="nav" aria-label="main piattaforma-notifiche sender">
+				{sections.map((section, index) => (
+					<Link
+						to={section.path}
+						key={index}
+						style={{ textDecoration: 'none' }}
+					>
+						<ListItemButton
+							selected={selectedIndex === index}
+							onClick={() => handleListItemClick(index)}
+						>
+							<ListItemIcon>{section.icon}</ListItemIcon>
+							<ListItemText primary={section.title} />
+						</ListItemButton>
+					</Link>
+				))}
+			</List>
+		</Box>
+	);
+};
