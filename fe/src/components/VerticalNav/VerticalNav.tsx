30c14cbb523ed32c9fd037e1b513ddda8c0c7b19
anislucidi01 <anis.lucidi@pagopa.it>
2023-05-30 11:27:44 +0200

    Implemented new sidebar version.

--- fe/src/components/VerticalNav/VerticalNav.tsx
+++ fe/src/components/VerticalNav/VerticalNav.tsx
@@ -1,21 +1,20 @@
-import React, { useEffect } from 'react';
-import { ThemeProvider } from "@mui/material";
-import { HorizontalNavProps, theme } from "@pagopa/mui-italia";
+import React, { useState } from 'react';
 import {
     List,
-    ListItem,
+    ListItemIcon,
     ListItemText,
     Box,
     ListItemButton,
-    useTheme,
 } from "@mui/material";
 import { Link } from 'react-router-dom';
+
 
 export interface Section {
     title: string;
     path: string;
     active: boolean
     element: JSX.Element
+    icon: JSX.Element
 }
 export interface VerticalNavProps {
     sections: Array<Section>;
@@ -25,54 +24,35 @@ export interface VerticalNavProps {
 
 
 export const VerticalNav = ({ sections, setSections, currentPath }: VerticalNavProps) => {
-    const theme = useTheme();
+    const [selectedIndex, setSelectedIndex] = useState(0);
 
-    /* I know this is not the best way, but for now I think it will do */
-    useEffect(() => {
-        sections.map((sec, ind) => {
-            if (sec.path === currentPath) {
-                setActiveSection(ind);
-            }
-        })
-    }, [currentPath])
-
-    const setActiveSection = (sectionIndex: number) => {
-        let sectionArray: Section[] = [];
-
-        sections.map((sec, index) => {
-            if (index === sectionIndex) {
-                sec.active = true;
-            }
-            else {
-                sec.active = false;
-            }
-            sectionArray.push(sec);
-
-        })
-
-        setSections(sectionArray);
-    }
+    const handleListItemClick = (index: number) => {
+        setSelectedIndex(index);
+    };
 
     return (
-        <ThemeProvider theme={theme}>
-            <Box sx={{ width: '100%', maxWidth: 360, float: 'left', marginRight: "10vw" }}>
-                <nav aria-label="main mailbox folders">
-                    <List sx={{ paddingTop: "0px" }}>
-                        {sections.map((section, index) => {
-                            const bgColor = section.active ? theme.palette.primary.light : theme.palette.primary.dark;
-                            return (
-                                <Link to={section.path} onClick={() => { setActiveSection(index) }} key={index} style={{ textDecoration: 'none' }}>
-                                    <ListItem disablePadding sx={{ bgcolor: bgColor, paddingTop: "0px" }} >
-                                        <ListItemButton>
-                                            <ListItemText primary={section.title} sx={{ color: "primary.contrastText", textAlign: 'center' }} />
-                                        </ListItemButton>
-                                    </ListItem>
-                                </Link>
-                            )
-                        })}
-                    </List>
-                </nav>
-            </Box>
-        </ThemeProvider >
-    )
-};
+        <Box sx={{ width: '100%', maxWidth: 360, float: 'left', marginRight: "10vw", backgroundColor: 'background.paper' }}>
+
+            <List component="nav" aria-label="main piattaforma-notifiche sender">
+                {
+                    sections.map((section, index) => (
+                        <Link to={section.path} key={index} style={{ textDecoration: 'none' }}>
+                            <ListItemButton
+                                selected={selectedIndex === index}
+                                onClick={() => handleListItemClick(index)}
+                            >
+                                <ListItemIcon>
+
+                                    {section.icon}
+                                </ListItemIcon>
+                                <ListItemText primary={section.title} />
+                            </ListItemButton>
+                        </Link>))
+                }
+
+            </List>
+        </Box>
+    );
+}
+
+
